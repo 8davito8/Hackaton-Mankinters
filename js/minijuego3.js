7538c2b31e8f99fe7624eaacb668c4c986032acd
josemizq <jose.miguel.izq@gmail.com>
2015-11-27 18:58:53 +0100

    backgrounds

--- js/minijuego3.js
+++ js/minijuego3.js
@@ -8,7 +8,7 @@
       this.input.onDown.add(this.onInputDown, this);
 
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.game.stage.backgroundColor = '#000000';
+        this.game.stage.backgroundColor = '#00FFFF';
 
         var bg = this.game.add.tileSprite(0, 0, 800, 600, 'background');
     },
