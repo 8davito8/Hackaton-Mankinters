ced509e86d39125346254adcdf5c7ccb12cf23e0
josemizq <jose.miguel.izq@gmail.com>
2015-11-27 19:14:48 +0100

    Menu colores

--- js/menu.js
+++ js/menu.js
@@ -18,13 +18,17 @@
         var juego3 = this.game.add.button(this.game.world.centerX - 95, 350, 'button', this.game3, this, 2, 1, 0);
         */
         
-        var juego1 = this.game.add.text(this.game.world.centerX - 95, 200, "MINIJUEGO1");
-        var juego2 = this.game.add.text(this.game.world.centerX - 95, 200, "MINIJUEGO2");
-        var juego3 = this.game.add.text(this.game.world.centerX - 95, 200, "MINIJUEGO3");
+        var juego1 = this.game.add.text(this.game.world.centerX - 95, 50, "MINIJUEGO1",{ font: "24px Arial", fill: "#FFF" });
+        var juego2 = this.game.add.text(this.game.world.centerX - 95, 200, "MINIJUEGO2",{ font: "24px Arial", fill: "#FFF" });
+        var juego3 = this.game.add.text(this.game.world.centerX - 95, 350, "MINIJUEGO3",{ font: "24px Arial", fill: "#FFF" });
         
-        juego1.input.onDown(function(){
-            this.game.state.start("minijuego1");
-        });
+        juego1.inputEnabled = true;
+        juego2.inputEnabled = true;
+        juego3.inputEnabled = true;
+        
+        juego1.events.onInputDown.add(this.game1, this);
+        juego2.events.onInputDown.add(this.game2, this);
+        juego3.events.onInputDown.add(this.game3, this);
         
         
     },
