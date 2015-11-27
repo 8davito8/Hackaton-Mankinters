7e6c1697db1fc675e884ca61eccbebab0139a4e9
josemizq <jose.miguel.izq@gmail.com>
2015-11-27 18:52:18 +0100

    minijuegos

---
+++ js/minijuego3.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  function Game() {}
+
+  minijuego3.prototype = {
+    create: function () {
+      this.input.onDown.add(this.onInputDown, this);
+
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.stage.backgroundColor = '#000000';
+
+        bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+    },
+
+    update: function () {
+
+    },
+
+    onInputDown: function () {
+      this.game.state.start('menu');
+    }
+  };
+
+  window['mankinters'] = window['mankinters'] || {};
+  window['mankinters'].Game = Game;
+}());
