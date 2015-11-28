26a50480593c1b8d7a9fe2c22b96282b354f7f66
josemizq <jose.miguel.izq@gmail.com>
2015-11-28 14:20:54 +0100

    qwsdfghj

--- js/minijuego1.js
+++ js/minijuego1.js
@@ -58,11 +58,11 @@
             this.game.physics.enable(player);
             this.game.physics.enable(player, Phaser.Physics.ARCADE);
 
-            player.body.bounce.y = 0.2;
+            player.body.bounce.y = 0.1;
             player.body.collideWorldBounds = true;
             player.body.setSize(20, 32, 5, 16);
 
-            player.body.gravity.y = 450;
+            player.body.gravity.y = 320;
 
             player.animations.add('left', [0, 1, 2, 3], 10, true);
             player.animations.add('turn', [4], 20, true);
