4e4295881dfd8ef98773d17af83784823bd2c90e
josemizq <jose.miguel.izq@gmail.com>
2015-11-28 01:20:45 +0100

    asdf

--- js/minijuego2.js
+++ js/minijuego2.js
@@ -4,22 +4,23 @@
   function minijuego2() {}
 
   minijuego2.prototype = {
+      
+    preload: function() {
+        
+    },
+      
     create: function () {
-      this.input.onDown.add(this.onInputDown, this);
-
+        
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.stage.backgroundColor = '#FF00FF';
 
-        //var bg = this.game.add.tileSprite(0, 0, 800, 600, 'background');
+
     },
 
     update: function () {
 
-    },
-
-    onInputDown: function () {
-      this.game.state.start('menu');
     }
+
   };
 
   window['mankinters'] = window['mankinters'] || {};
